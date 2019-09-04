dc87917c74b8764fad7d2be3ae98c1f8202d18db
Mitchell <soggybag@gmail.com>
2019-09-04 11:35:09 -0700

    Updates

---
+++ precache-manifest.ab0eba79eb73e2592be26f31cc45a670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d69c27d07698fe39283ea1e34622a3",
+    "url": "/tetris-redux-app/index.html"
+  },
+  {
+    "revision": "4fa49f5f11a4c7e31048",
+    "url": "/tetris-redux-app/static/css/main.7e39fefd.chunk.css"
+  },
+  {
+    "revision": "e5086004b4cf0d2e2bb0",
+    "url": "/tetris-redux-app/static/js/2.2622b67b.chunk.js"
+  },
+  {
+    "revision": "4fa49f5f11a4c7e31048",
+    "url": "/tetris-redux-app/static/js/main.05e25556.chunk.js"
+  },
+  {
+    "revision": "1e1f5882f3cab54da262",
+    "url": "/tetris-redux-app/static/js/runtime~main.9f8aec96.js"
+  }
+]);
